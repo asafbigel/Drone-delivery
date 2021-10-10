5cbfc1474411c819539b384122d2d602bbad6ef6
Asaf-Bigel <bigel@g.jct.ac.il>
2021-10-10 09:09:52 +0300

    Inital code + Welecome function

---
+++ Targil0/Program3571.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welecome3571();
+            Welecome9647();
+            Console.ReadKey();
+        }
+
+        static partial void Welecome9647();
+
+        private static void Welecome3571()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
