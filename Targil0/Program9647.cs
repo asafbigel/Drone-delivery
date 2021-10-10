876c2c172f7750eb3a8c0a935c2798d180eb3f31
menachem <menaori1@gmail.com>
2021-10-10 10:02:35 +0300

    Initial code+Welcome functions

---
+++ Targil0/Program9647.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static partial void Welecome9647()
+        {
+            Console.WriteLine("abc");
+        }
+    }
+}
