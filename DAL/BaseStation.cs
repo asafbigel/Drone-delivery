770ffbc4c36616f75027cc24aa82d9ba6babdcbd
Asaf-Bigel <bigel@g.jct.ac.il>
2021-10-20 14:16:21 +0300

    Add the struct (BaseStation, DroneCharge, Drone, Parcel, Customer)

---
+++ DAL/BaseStation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        struct BaseStation
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public double Longitude { get; set; }
+            public double Lattitude { get; set; }
+            public int ChargeSlots { get; set; }
+            public override string ToString()
+            {
+                string result = "";
+                result+= $"Id is {Id}\n";
+            }
+        }
+
+        struct DroneCharge
+        {
+            public int DroneId { get; set; }
+            public int StationId { get; set; }
+        }
+
+        struct Drone
+        {
+            public int Id { get; set; }
+            public string Model { get; set; }
+            public WeightCategories MaxWeight { get; set; }
+            public DroneStatuses Status { get; set; }
+            public double Battery { get; set; }
+        }
+
+        struct Parcel
+        {
+            public int Id { get; set; }
+            public int senderId { get; set; }
+            public int targetId { get; set; }
+            public WeightCategories Weight { get; set; }
+            public Priorities Priority { get; set; }
+            public int DroneId { get; set; }
+            public DateTime Scheduled { get; set; }
+            public DateTime PickedUp { get; set; }
+            public DateTime Delivered { get; set; }
+        }
+
+        struct Customer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Phone { get; set; }
+            public double Longitude { get; set; }
+            public double Latitude { get; set; }
+        }
+    }
+}
