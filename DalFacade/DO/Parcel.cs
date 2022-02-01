87245391a27d1edf1e75e785c13d904ff818ee07
menachem <menaori1@gmail.com>
2022-02-01 15:41:16 +0200

    a

--- DalFacade/DO/Parcel.cs
+++ DalFacade/DO/Parcel.cs
@@ -4,40 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-    namespace DO
-    {
+namespace DO
+{
 
-        public struct Parcel
+    public struct Parcel
+    {
+        public int Id { get; set; }
+        public int SenderId { get; set; }
+        public int TargetId { get; set; }
+        public bool CollectionConfirmation  { get; set; }        
+        public bool CollectionRecive { get; set; }
+        public WeightCategories Weight { get; set; }
+        public Priorities Priority { get; set; }
+        public int DroneId { get; set; }
+
+        // Time of created the request
+        public DateTime? Requested { get; set; }
+        // Time of connected between the parcel and a drone
+        public DateTime? Scheduled { get; set; }
+        // Time of taked the parcel from the sender
+        public DateTime? PickedUp { get; set; }
+        // Time of getted the parcel to the sender
+        public DateTime? Delivered { get; set; }
+        public override string ToString()
         {
-            public int Id { get; set; }
-            public int SenderId { get; set; }
-            public int TargetId { get; set; }
-            public WeightCategories Weight { get; set; }
-            public Priorities Priority { get; set; }
-            public int DroneId { get; set; }
-            
-            // Time of created the request
-            public DateTime? Requested { get; set; }
-            // Time of connected between the parcel and a drone
-            public DateTime? Scheduled { get; set; }
-            // Time of taked the parcel from the sender
-            public DateTime? PickedUp { get; set; }
-            // Time of getted the parcel to the sender
-            public DateTime? Delivered { get; set; }
-            public override string ToString()
-            {
-                string result = "";
-                result += "ID: " + Id;
-                result += " SenderId: " + SenderId;
-                result += " TargetId: " + TargetId;
-                result += " Weight: " + Weight;
-                result += " Priority: " + Priority;
-                result += " DroneId: " + DroneId;
-                result += " Requested: " + Requested;
-                result += " Scheduled: " + Scheduled;
-                result += " PickedUp: " + PickedUp;
-                result += " Delivered: " + Delivered + '\n';
-                return result;
-            }
+            string result = "";
+            result += "ID: " + Id;
+            result += " SenderId: " + SenderId;
+            result += " TargetId: " + TargetId;
+            result += " Weight: " + Weight;
+            result += " Priority: " + Priority;
+            result += " DroneId: " + DroneId;
+            result += " Requested: " + Requested;
+            result += " Scheduled: " + Scheduled;
+            result += " PickedUp: " + PickedUp;
+            result += " Delivered: " + Delivered + '\n';
+            return result;
         }
     }
+}
